2f7eba84c787c21372ecebb15ec068a9b6ddde53
eko <febri1412@gmail.com>
2019-07-22 21:21:53 +0700

    inital commit

---
+++ server.js
@@ -0,0 +1,46 @@
+require('dotenv').config()
+const express = require('express')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+const helmet = require('helmet')
+
+const Joi = require('@hapi/joi')
+Joi.objectId = require('joi-objectid')(Joi)
+
+const app = express()
+
+app.use(logger('dev'))
+app.use(helmet.xssFilter())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+const config = require('config')
+
+const mongoose = require('mongoose')
+
+if (!config.get('PrivateKey')) {
+  console.error('FATAL ERROR: PrivateKey is not defined.')
+  process.exit(1)
+}
+
+mongoose.set('useCreateIndex', true)
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  dbName: process.env.MONGO_DBNAME
+}).then(() => {
+  console.log('connection success')
+}).catch(err => {
+  console.log(`connection error `, err)
+  process.exit()
+})
+
+// public routes
+app.get('/', (req, res) => {
+  res.json({ message: 'server running' })
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`server running in port ${port}`)
+})
